00d1709475e337ee385d6df5f5fe0fa3fd5a16ba
geettalati <2340401112@stu.manit.ac.in>
2025-12-10 00:01:17 +0530

    changes in layout and logic

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
         const apikey = "4beaca7cbb90efc054aeb77958dc6c14";
-        const apiurl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=bangalore";
-        
-        async function checkweather(){
-            const response = await fetch(apiurl + `&appid=${apikey}`);
-            var data = await response.json();
-            console.log(data);
-        }
-        checkweather();
+        const apiurl = "https://api.openweathermap.org/data/2.5/weather?&units=metric";
 
-        document.querySelector(".city").innerHTML = data.name;
-        document.querySelector(".temp").innerHTML = data.main.temp;
+        const searchbox = document.querySelector(".search input");
+        const searchbtn = document.querySelector(".search button");
+        const weatherimg = document.querySelector(".weather-icon");
+        
+        async function checkweather(city){
+            const response = await fetch(apiurl + city + `&appid=${apikey}`);
+
+            if(response.status == 404){
+                document.querySelector(".error").style.display = "block";
+                document.querySelector(".weather").style.display = "none";
+            }
+            else{
+            var data = await response.json();
+            
+            document.querySelector(".city").innerHTML = data.name;
+            document.querySelector(".temp").innerHTML = math.round(data.main.temp) + "C";
+            document.querySelector(".humidity").innerHTML = data.main.humidity + "% ";
+            document.querySelector(".wind").innerHTML = data.main.wind + "KM/H";
+
+            if( data.weather[0].main == "Clouds"){
+                weatherimg.src = "images/clouds.png";
+
+            }
+            else if( data.weather[0].main == "Rain"){
+                weatherimg.src = "images/rain.png";
+            }
+            else if( data.weather[0].main == "Drizzle"){
+                weatherimg.src = "images/drizzle.png";
+            }
+            else if( data.weather[0].main == "Mist"){
+                weatherimg.src = "images/mist.png";
+            }
+            document.querySelector(".weather").style.display = "block";
+            document.querySelector(".error").style.display = "none";
+            }
+        }
+        searchbox.addEventListener("click", ()=>{
+            checkweather(searchbox.value);
+        })
+        
+
+
+        
+        
